f07e9f13ac1dff8fa3566d58ce53ee2ec68efdff
Antonio obaid <abo.george11@hotmail.com>
2025-10-06 22:05:24 +0200

    kunfigrera tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,16 @@
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-<<<<<<< HEAD
+
      <label htmlFor="">dfsf</label>
      <button>tony</button>
      <input type="text" />
-=======
+
       <button className="p-8">
         <input className="" type="text" />
         tony
       </button>
->>>>>>> 0c64e0e (Initial commit – ChiqueButik projekt)
+
     </div>
   );
 }
